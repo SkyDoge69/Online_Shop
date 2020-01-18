1667a83194e49784067a9b11a572d7eb9576ffaf
SkyDoge69 <luc2002penta@gmail.com>
2020-01-18 21:20:08 +0200

    Changed names and added ad attributes

---
+++ model/ad.py
@@ -0,0 +1,64 @@
+#from database import SQLite
+#from errors import ApplicationError
+
+class Ad(object):
+
+    def __init__(self, title, content, price, release_date, is_active, buyer, ad_id=None):
+        self.id = ad_id
+        self.title = title
+        self.content = content
+        self.price = price
+        self.release_date = release_date
+        self.is_active = is_active
+        self.buyer = buyer
+
+    def to_dict(self):
+        return self.__dict__
+
+    def save(self):
+        with SQLite() as db:
+            cursor = db.execute(self.__get_save_query())
+            self.id = cursor.lastrowid
+        return self
+
+    @staticmethod
+    def delete(ad_id):
+        result = None
+        with SQLite() as db:
+            result = db.execute("DELETE FROM ad WHERE id = ?",
+                    (ad_id,))
+        if result.rowcount == 0:
+            raise ApplicationError("No value present", 404)
+
+    @staticmethod
+    def find(ad_id):
+        result = None
+        with SQLite() as db:
+            result = db.execute(
+                    "SELECT title, content, id FROM ad WHERE id = ?",
+                    (ad_id,))
+        ad = result.fetchone()
+        if ad is None:
+            raise ApplicationError(
+                    "Ad with id {} not found".format(ad_id), 404)
+        return Ad(*ad)
+
+    @staticmethod
+    def all():
+        with SQLite() as db:
+            result = db.execute(
+                    "SELECT title, content, id FROM ad").fetchall()
+            return [Ad(*row) for row in result]
+
+    def __get_save_query(self):
+        query = "{} INTO ad {} VALUES {}"
+        if self.id == None:
+            args = (self.title, self.content)
+            query = query.format("INSERT", "(title, content)", args)
+        else:
+            args = (self.id, self.title, self.content)
+            query = query.format("REPLACE", "(id, title, content)", args)
+        return query
+
+
+
